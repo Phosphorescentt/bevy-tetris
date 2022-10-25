ca3f77a68b44a9fd1bf18677bd64617f0a662727
Joshua Mankelow <joshua@mankelow.org>
2022-10-25 08:52:19 +0100

    Added clickable menu button B)

--- src/in_game/mod.rs
+++ src/in_game/mod.rs
@@ -8,11 +8,18 @@ struct UIEntity {
     text_entity: Entity,
 }
 
+struct PauseUIEntity {
+    text_entity: Entity,
+}
+
 impl Plugin for InGamePlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup_ui))
             .add_system_set(SystemSet::on_update(AppState::InGame).with_system(run_ui))
-            .add_system_set(SystemSet::on_exit(AppState::InGame).with_system(cleanup_ui));
+            .add_system_set(SystemSet::on_exit(AppState::InGame).with_system(cleanup_ui))
+            .add_system_set(SystemSet::on_enter(AppState::Paused).with_system(setup_pause_ui))
+            .add_system_set(SystemSet::on_update(AppState::Paused).with_system(run_pause_ui))
+            .add_system_set(SystemSet::on_exit(AppState::Paused).with_system(cleanup_pause_ui));
     }
 }
 
@@ -31,12 +38,48 @@ fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(UIEntity { text_entity });
 }
 
-fn run_ui(keys: Res<Input<KeyCode>>, mut app_state: ResMut<State<AppState>>) {
-    if keys.just_pressed(KeyCode::P) {
+fn run_ui(mut keys: ResMut<Input<KeyCode>>, mut app_state: ResMut<State<AppState>>) {
+    if keys.just_pressed(KeyCode::Escape) {
         let _ = app_state.set(AppState::MainMenu);
+        keys.clear();
+    }
+
+    if keys.just_pressed(KeyCode::P) {
+        let _ = app_state.push(AppState::Paused).unwrap();
+        keys.clear();
     }
 }
 
 fn cleanup_ui(mut commands: Commands, ui_entity: Res<UIEntity>) {
     commands.entity(ui_entity.text_entity).despawn_recursive();
+}
+
+fn setup_pause_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let ui_text_entity = commands
+        .spawn_bundle(TextBundle::from_section(
+            "Paused",
+            TextStyle {
+                font: asset_server.load("fonts/Silkscreen-Regular.ttf"),
+                font_size: 60.0,
+                color: Color::WHITE,
+            },
+        ))
+        .id();
+
+    commands.insert_resource(PauseUIEntity {
+        text_entity: ui_text_entity,
+    });
+}
+
+fn run_pause_ui(mut keys: ResMut<Input<KeyCode>>, mut app_state: ResMut<State<AppState>>) {
+    if keys.just_pressed(KeyCode::Escape) {
+        let _ = app_state.pop().unwrap();
+        keys.clear();
+    }
+}
+
+fn cleanup_pause_ui(mut commands: Commands, pause_ui_entity: Res<PauseUIEntity>) {
+    commands
+        .entity(pause_ui_entity.text_entity)
+        .despawn_recursive();
 }
--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -6,12 +6,21 @@ pub struct MainMenuPlugin;
 
 struct MenuEntity {
     text_entity: Entity,
+    button_entity: Entity,
 }
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.7, 0.35);
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(AppState::MainMenu).with_system(setup_menu))
-            .add_system_set(SystemSet::on_update(AppState::MainMenu).with_system(run_menu))
+            .add_system_set(
+                SystemSet::on_update(AppState::MainMenu)
+                    .with_system(run_menu)
+                    .with_system(button_cosmetic_system),
+            )
             .add_system_set(SystemSet::on_exit(AppState::MainMenu).with_system(cleanup_menu));
     }
 }
@@ -28,15 +37,74 @@ fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
         ))
         .id();
 
-    commands.insert_resource(MenuEntity { text_entity });
+    let button_entity = commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                margin: UiRect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle::from_section(
+                "Button",
+                TextStyle {
+                    font: asset_server.load("fonts/Silkscreen-Regular.ttf"),
+                    font_size: 40.0,
+                    color: Color::rgb(0.9, 0.9, 0.9),
+                },
+            ));
+        })
+        .id();
+
+    commands.insert_resource(MenuEntity {
+        text_entity,
+        button_entity,
+    });
 }
 
-fn run_menu(keys: Res<Input<KeyCode>>, mut app_state: ResMut<State<AppState>>) {
-    if keys.just_pressed(KeyCode::O) {
+fn run_menu(mut keys: ResMut<Input<KeyCode>>, mut app_state: ResMut<State<AppState>>) {
+    if keys.just_pressed(KeyCode::Space) {
         let _ = app_state.set(AppState::InGame);
+        keys.clear();
     }
 }
 
 fn cleanup_menu(mut commands: Commands, menu_entity: Res<MenuEntity>) {
     commands.entity(menu_entity.text_entity).despawn_recursive();
+    commands
+        .entity(menu_entity.button_entity)
+        .despawn_recursive();
+}
+
+fn button_cosmetic_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut app_state: ResMut<State<AppState>>,
+) {
+    //
+    for (interaction, mut color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "WOOOOOOO!".to_string();
+                *color = PRESSED_BUTTON.into();
+                let _ = app_state.set(AppState::InGame);
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Play!".to_string();
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
 }
